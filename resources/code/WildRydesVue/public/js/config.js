1eaad9d599e4a3ddc7adfe6a09c7c266ffec6e35
Rob Sutter <robsut@amazon.com>
2020-10-09 16:27:10 -0400

    Corrected .gitignore in root that was blocking required Vue public/ directory

---
+++ resources/code/WildRydesVue/public/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_PPkynf3wh', // e.g. eu-east-2_uXboG5pAb
+        userPoolClientId: '16kjssnfp8bm7v6ugksic72rl0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
