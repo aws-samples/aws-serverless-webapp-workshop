596d5229da4f7d541ced2d30ff98e91602e413b1
Ben Smith <benjasl@amazon.com>
2023-06-15 09:13:22 +0100

    Update requestUnicorn.js

--- workshop/content/3-serverlessbackend/4-lambda/requestUnicorn.js
+++ workshop/content/3-serverlessbackend/4-lambda/requestUnicorn.js
@@ -1,115 +1,99 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-const randomBytes = require('crypto').randomBytes;
+const { randomBytes } = require('crypto');
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+const { marshall } = require('@aws-sdk/util-dynamodb');
 
-const AWS = require('aws-sdk');
-
-const ddb = new AWS.DynamoDB.DocumentClient();
+const ddbClient = new DynamoDBClient({ region: 'us-east-1' }); // Update with your desired region
 
 const fleet = [
-    {
-        Name: 'Bucephalus',
-        Color: 'Golden',
-        Gender: 'Male',
-    },
-    {
-        Name: 'Shadowfax',
-        Color: 'White',
-        Gender: 'Male',
-    },
-    {
-        Name: 'Rocinante',
-        Color: 'Yellow',
-        Gender: 'Female',
-    },
+  {
+    Name: 'Bucephalus',
+    Color: 'Golden',
+    Gender: 'Male',
+  },
+  {
+    Name: 'Shadowfax',
+    Color: 'White',
+    Gender: 'Male',
+  },
+  {
+    Name: 'Rocinante',
+    Color: 'Yellow',
+    Gender: 'Female',
+  },
 ];
 
-exports.handler = (event, context, callback) => {
-    if (!event.requestContext.authorizer) {
-      errorResponse('Authorization not configured', context.awsRequestId, callback);
-      return;
-    }
+exports.handler = async (event) => {
+  if (!event.requestContext.authorizer) {
+    return errorResponse('Authorization not configured', event.requestContext.requestId);
+  }
 
-    const rideId = toUrlString(randomBytes(16));
-    console.log('Received event (', rideId, '): ', event);
+  const rideId = toUrlString(randomBytes(16));
+  console.log('Received event (', rideId, '): ', event);
 
-    // Because we're using a Cognito User Pools authorizer, all of the claims
-    // included in the authentication token are provided in the request context.
-    // This includes the username as well as other attributes.
-    const username = event.requestContext.authorizer.claims['cognito:username'];
+  const username = event.requestContext.authorizer.claims['cognito:username'];
 
-    // The body field of the event in a proxy integration is a raw string.
-    // In order to extract meaningful values, we need to first parse this string
-    // into an object. A more robust implementation might inspect the Content-Type
-    // header first and use a different parsing strategy based on that value.
-    const requestBody = JSON.parse(event.body);
+  const requestBody = JSON.parse(event.body);
 
-    const pickupLocation = requestBody.PickupLocation;
+  const pickupLocation = requestBody.PickupLocation;
 
-    const unicorn = findUnicorn(pickupLocation);
+  const unicorn = findUnicorn(pickupLocation);
 
-    recordRide(rideId, username, unicorn).then(() => {
-        // You can use the callback function to provide a return value from your Node.js
-        // Lambda functions. The first parameter is used for failed invocations. The
-        // second parameter specifies the result data of the invocation.
+  try {
+    await recordRide(rideId, username, unicorn);
 
-        // Because this Lambda function is called by an API Gateway proxy integration
-        // the result object must use the following structure.
-        callback(null, {
-            statusCode: 201,
-            body: JSON.stringify({
-                RideId: rideId,
-                Unicorn: unicorn,
-                UnicornName: unicorn.Name,
-                Eta: '30 seconds',
-                Rider: username,
-            }),
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-        });
-    }).catch((err) => {
-        console.error(err);
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        RideId: rideId,
+        Unicorn: unicorn,
+        UnicornName: unicorn.Name,
+        Eta: '30 seconds',
+        Rider: username,
+      }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    };
+  } catch (err) {
+    console.error(err);
 
-        // If there is an error during processing, catch it and return
-        // from the Lambda function successfully. Specify a 500 HTTP status
-        // code and provide an error message in the body. This will provide a
-        // more meaningful error response to the end client.
-        errorResponse(err.message, context.awsRequestId, callback)
-    });
+    return errorResponse(err.message, event.requestContext.requestId);
+  }
 };
 
-// This is where you would implement logic to find the optimal unicorn for
-// this ride (possibly invoking another Lambda function as a microservice.)
-// For simplicity, we'll just pick a unicorn at random.
 function findUnicorn(pickupLocation) {
-    console.log('Finding unicorn for ', pickupLocation.Latitude, ', ', pickupLocation.Longitude);
-    return fleet[Math.floor(Math.random() * fleet.length)];
+  console.log('Finding unicorn for ', pickupLocation.Latitude, ', ', pickupLocation.Longitude);
+  return fleet[Math.floor(Math.random() * fleet.length)];
 }
 
-function recordRide(rideId, username, unicorn) {
-    return ddb.put({
-        TableName: 'Rides',
-        Item: {
-            RideId: rideId,
-            User: username,
-            Unicorn: unicorn,
-            UnicornName: unicorn.Name,
-            RequestTime: new Date().toISOString(),
-        },
-    }).promise();
+async function recordRide(rideId, username, unicorn) {
+  const params = {
+    TableName: 'Rides',
+    Item: marshall({
+      RideId: rideId,
+      User: username,
+      Unicorn: unicorn,
+      UnicornName: unicorn.Name,
+      RequestTime: new Date().toISOString(),
+    }),
+  };
+
+  await ddbClient.send(new PutItemCommand(params));
 }
 
 function toUrlString(buffer) {
-    return buffer.toString('base64')
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=/g, '');
+  return buffer
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '');
 }
 
-function errorResponse(errorMessage, awsRequestId, callback) {
-  callback(null, {
+function errorResponse(errorMessage, awsRequestId) {
+  return {
     statusCode: 500,
     body: JSON.stringify({
       Error: errorMessage,
@@ -118,5 +102,5 @@ function errorResponse(errorMessage, awsRequestId, callback) {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-  });
+  };
 }
